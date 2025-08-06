f113deb57f555ef4783b08b3fd437896bc1088b2
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-08-06 17:48:30 +0800

    pnpm@10.14.0

--- rsg-less-2025-aug/set-env.sh
+++ rsg-less-2025-aug/set-env.sh
@@ -16,7 +16,7 @@ echo "                                                                          
 echo "###################################################################################################################"
 
 echo "Setting up PNPM..."
-corepack prepare pnpm@10.13.1 --activate
-corepack use pnpm@10.13.1
+corepack prepare pnpm@10.14.0 --activate
+corepack use pnpm@10.14.0
 pnpm --frozen-lockfile recursive install
 
