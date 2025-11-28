9ec47baa7ac5a0d2a25528c49e8571b052ca4334
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-11-28 11:43:54 +0800

    pnpm@10.24.0 nodejs-slim@24.11.1

--- rsg-less-2025-aug/set-env.sh
+++ rsg-less-2025-aug/set-env.sh
@@ -16,7 +16,7 @@ echo "                                                                          
 echo "###################################################################################################################"
 
 echo "Setting up PNPM..."
-corepack prepare pnpm@10.19.0 --activate
-corepack use pnpm@10.19.0
+corepack prepare pnpm@10.24.0 --activate
+corepack use pnpm@10.24.0
 pnpm --frozen-lockfile recursive install
 
